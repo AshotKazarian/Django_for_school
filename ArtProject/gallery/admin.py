8c50679f31ccc00921b268f056d6060dba063aed
AshotKazarian <103939529+AshotKazarian@users.noreply.github.com>
2025-12-01 20:16:02 +0300

    Update admin.py

--- ArtProject/gallery/admin.py
+++ ArtProject/gallery/admin.py
@@ -80,15 +80,3 @@ class WorkAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         """Оптимизация запросов к базе данных"""
         return super().get_queryset(request).select_related('author', 'genre', 'proportion').prefetch_related('tools')
-        
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    """Админка для профилей"""
-    list_display = ('user', 'bio_preview')
-    search_fields = ('user__username', 'bio')
-    list_per_page = 20
-    
-    def bio_preview(self, obj):
-        return obj.bio[:50] + '...' if obj.bio and len(obj.bio) > 50 else obj.bio
-    bio_preview.short_description = 'О себе (превью)'
